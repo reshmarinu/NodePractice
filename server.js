38b5293662edf392ec930ba2736dc1bee5e7e628
reshmarinu <reshmaannarajan1@gmail.com>
2021-02-04 01:39:04 +0530

    Added Hello.js Route

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port    = 3000
 app.get('/api',(req,res)=>{
     res.send('Hello World')
 })
+
+//Registering API Endpoint
+app.use('/hello', require('../NodePractice/routes/public/hello'))
+
+
 // Create server .
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
